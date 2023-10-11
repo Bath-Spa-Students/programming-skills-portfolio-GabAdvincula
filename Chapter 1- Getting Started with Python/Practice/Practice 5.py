447fa91e7726f6f6e5025dc695266635406b094d
GabAdvincula <gabzzadvincula@gmail.com>
2023-10-11 19:21:50 +0400

    Practice 5.py

--- Chapter 1- Getting Started with Python/Practice/Practice 5.py
+++ Chapter 1- Getting Started with Python/Practice/Practice 5.py
@@ -1,1 +1,7 @@
-#Q5:Write four strings in different variables and print the output as one string.(String Concatenation)
+name1 ="Gabriel"
+name2 ="Jude"
+name3 ="Andre"
+name4 ="Advincula"
+
+names = name1 + " " + name2 + " " + name3 + " " + name4
+print  (names)
