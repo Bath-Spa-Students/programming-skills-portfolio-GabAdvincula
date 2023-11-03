3696f7a5110ae1c214162f77a107979840568a16
Gabriel Advincula <144330055+GabAdvincula@users.noreply.github.com>
2023-11-03 15:39:20 +0400

    Update and rename Chapter 3_Exercise 6.py to CH3_Exercise 6.py

--- Chapter 3- Data Structures/Exercises/CH3_Exercise 6.py
+++ Chapter 3- Data Structures/Exercises/CH3_Exercise 6.py
@@ -1,3 +1,6 @@
+#You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.]
+#Start with your program from Exercise 3-5. Add a new line that prints a message saying that you can invite only two people for dinner.
+
 guest = ['Angelo', 'Simon', 'Johnlloyd', 'Luke']
 
 inv1 = f"{guest[0]}, are you free for dinner this evening?"
@@ -14,7 +17,7 @@ print(inv5)
 
 del guest[2]
 guest.insert(2, 'Mark')
-
+#You just heard that Johnlloyd can’t make it to the dinner, so you need to send out a new set of invitations. You invited Mark instead.
 inv1 = f"\n{guest[0]}, are you free for dinner this evening?"
 inv2 = f"{guest[1]}, are you free for dinner this evening"
 inv3 = f"{guest[2]}, are you free for dinner this evening"
@@ -24,10 +27,11 @@ print(inv1)
 print(inv2)
 print(inv3)
 print(inv4)
-
+#You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
 msg = "\nI'm sorry, but I can only invite two individuals because the dinner table seems to be having some issues and won't arrive in time."
 print(msg)
 
+#Use pop() to remove guests from your list one at a time until only two names remain in your list. 
 apology1 = f"\nI apologize {guest[0]}, but unfortunately there is only space for two persons."
 inv1 = guest.pop(0)
 apology2 = f"I apologize {guest[0]}, but unfortunately there is only space for two persons."
@@ -42,7 +46,7 @@ inv4 = f"{guest[1]},you're still invited to dinner."
 print(inv3)
 print(inv4)
 
-
+#Use del to remove the last two names from your list, so you have an empty list. 
+#Print your list to make sure you actually have an empty list at the end of your program.
 del(guest[0] , guest[0])
-
 print(guest)
