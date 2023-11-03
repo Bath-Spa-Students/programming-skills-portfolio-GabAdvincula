7b0fdf2caae7bf0d68a0cff2aad9ed901a05bf35
Gabriel Advincula <144330055+GabAdvincula@users.noreply.github.com>
2023-11-03 15:20:31 +0400

    Update and rename Chapter2_Exercise 1.py to CH2_Exercise 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/CH2_Exercise 1.py
@@ -0,0 +1,7 @@
+#Assign a message to a variable, and print that message.
+#Then change the value of the variable to a new message, and print the new message.
+
+text = "Nice to meet you!"
+print(text)
+text = "By the way my name is Gab"
+print(text)
--- Chapter 2- Variables & Comments/Exercises/Chapter2_Exercise 1.py
+++
@@ -1,4 +0,0 @@
-text = "Nice to meet you!"
-print(text)
-text = "By the way my name is Gab"
-print(text)
