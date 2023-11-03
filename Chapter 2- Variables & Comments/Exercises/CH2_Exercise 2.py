979639eeaf7b683d05a26f7a2e022f90853d8d0a
Gabriel Advincula <144330055+GabAdvincula@users.noreply.github.com>
2023-11-03 15:21:40 +0400

    Update and rename Chapter2_Exercise 2.py to CH2_Exercise 2.py

--- Chapter 2- Variables & Comments/Exercises/CH2_Exercise 2.py
+++ Chapter 2- Variables & Comments/Exercises/CH2_Exercise 2.py
@@ -1,2 +1,5 @@
+#Variables
+#Find a quote from a famous person you admire. Print the quote and the name of its author. 
+
 quote =  (""" "That's one small step for a man, a giant leap for mankind." """)
 print(f"Neil Armstrong once said,{quote}")
