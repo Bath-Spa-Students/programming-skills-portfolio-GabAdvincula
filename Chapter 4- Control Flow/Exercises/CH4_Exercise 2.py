646e4b6481ed82f4be077961154a7849afb84d05
Gabriel Advincula <144330055+GabAdvincula@users.noreply.github.com>
2023-11-03 15:47:17 +0400

    Update and rename Exercise 2.py to CH4_Exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/CH4_Exercise 2.py
@@ -0,0 +1,15 @@
+# If block statement
+
+alien_color = 'green'
+if alien_color == 'green':
+   print("Congratulations , you've just earned 5 points for shooting the alien!!!")
+else: 
+   print("Congratulations , you've just earned 10 points!!!")
+
+# Else block statement
+
+alien_color = 'red'
+if alien_color == 'green':
+   print("Congratulations , you've just earned 5 points for shooting the alien!!!")
+else: 
+   print("Congratulations , you've just earned 10 points!!!")
