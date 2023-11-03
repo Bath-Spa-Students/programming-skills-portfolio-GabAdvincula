d84a8390283d0ee53d120e996dd4c83abe05260d
Gabriel Advincula <144330055+GabAdvincula@users.noreply.github.com>
2023-11-03 15:33:06 +0400

    Update and rename Chapter 3_Exercise 3.py to CH3_Exercise 3.py

--- Chapter 3- Data Structures/Exercises/CH3_Exercise 3.py
+++ Chapter 3- Data Structures/Exercises/CH3_Exercise 3.py
@@ -1,3 +1,6 @@
+#Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples.
+#Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
 transpo = ['Kawasaki 650', 'BMW M4 Sedan', 'Toyota Landcruiser 300','Lamborghini Huracan' , 'Nissan GTR R35' , 'Honda Civic']
 
 msg1 = f""" "My goal is to buy a {transpo[0]} once I get my license." """
