f775b56b7d1399fac4034250361a758a1c2dc221
Gabriel Advincula <144330055+GabAdvincula@users.noreply.github.com>
2023-11-03 15:26:30 +0400

    Update and rename Chapter2_Exercise 5.py to CH2_Exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/CH2_Exercise 5.py
@@ -0,0 +1,8 @@
+#A girl heads to a computer shop to buy some USB sticks. She loves USB sticks and wants as many as she can get for £50. They are £6 each.
+#Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left.
+#You will to use the arithmetic operators to complete this exercise.
+
+Usb_quantity = "Total number of use bought: "
+Remaining_Amount = "Amount left: "
+print (Usb_quantity + str(int(50/6)))
+print(Remaining_Amount + str((50%6)))
--- Chapter 2- Variables & Comments/Exercises/Chapter2_Exercise 5.py
+++
@@ -1,4 +0,0 @@
-Usb_quantity = "Total number of use bought: "
-Remaining_Amount = "Amount left: "
-print (Usb_quantity + str(int(50/6)))
-print(Remaining_Amount + str((50%6)))
