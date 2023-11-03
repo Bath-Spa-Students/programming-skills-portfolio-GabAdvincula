d10934212007e308a5a5fec5e087d640761e7f0e
Gabriel Advincula <144330055+GabAdvincula@users.noreply.github.com>
2023-11-03 15:34:35 +0400

    Update and rename Chapter 3_Exercise 4.py to CH3_Exercise 4.py

--- Chapter 3- Data Structures/Exercises/CH3_Exercise 4.py
+++ Chapter 3- Data Structures/Exercises/CH3_Exercise 4.py
@@ -1,5 +1,8 @@
+#If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner.
+
 guest= [ 'Angelo' , 'Simon' , 'Johnlloyd' , 'Luke']
 
+#Then use your list to print a message to each person, inviting them to dinner.
 inv1 = f"{guest[0]}, are you free for dinner this evening?"
 inv2 = f"{guest[1]}, are you free for dinner this evening?" 
 inv3 = f"{guest[2]}, are you free for dinner this evening?"
