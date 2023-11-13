f94c19672705ba182f183e76149a6fefcec49b46
Gabriel Advincula <144330055+GabAdvincula@users.noreply.github.com>
2023-11-13 21:55:08 +0400

    Update CH5_Exercise 4.py

--- Chapter 5- Dictionaries/Exercises/CH5_Exercise 4.py
+++ Chapter 5- Dictionaries/Exercises/CH5_Exercise 4.py
@@ -9,11 +9,11 @@ for river, country in major_rivers.items():
     print(f"The {river} runs through {country}.")
 
 # Print the name of each river
-print("\nThis data  includes the following rivers:")
+print("\nTRivers included in the dictionary:")
 for river in major_rivers.keys():
     print(river)
 
 # Print the name of each country
-print("\nThis data  includes the following countries:")
+print("\nCountries included in the dictionary:")
 for country in major_rivers.values():
     print(country)
